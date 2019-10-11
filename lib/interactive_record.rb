0799a56c11ac5190d46c13ee7acabcb095f99dae
Doug Kintop <dkintop@hotmail.com>
2019-10-11 17:50:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,7 +31,7 @@ class InteractiveRecord
   
   def col_names_for_insert
     
-    self.class.column_names
+    self.class.column_names.delete_if {|column_name| column_name == "id"}.join (", ")
     
   end 
   
